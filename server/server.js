f25c7eb278a976b6f8c56ddd4a4f289af385f8b6
Max <Hunt>
2021-12-24 23:24:06 +0000

    Update

--- server/server.js
+++ server/server.js
@@ -10,70 +10,59 @@ Contract.setProvider(infuraWsUrl);
 
 const app = express();
 const web3 = new Web3(infuraUrl)
-    // const web3_ws = new Web3(infuraWsUrl)
 
 
-var admin = require("firebase-admin");
+var firebase_admin = require("firebase-admin");
 var serviceAccount = require("../secrets/firebase_creds.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+firebase_admin.initializeApp({
+    credential: firebase_admin.credential.cert(serviceAccount),
     databaseURL: "https://polypaint-io-default-rtdb.europe-west1.firebasedatabase.app/"
 });
+var database = firebase_admin.database();
+var PIXEL_TABLE = database.ref("Pixels");
+var CACHED_DATABASE = []
+
+async function downloadDatabase() {
+    PIXEL_TABLE.once("value", function(snapshot) {
+        CACHED_DATABASE = snapshot.val();
+        console.log("Initial database download complete");
+    });
+}
+downloadDatabase()
+
 
 async function initWeb3() {
     const networkId = await web3.eth.net.getId()
-    console.log(contract.networks[networkId].address);
     var deployedContract = new Contract(contract.abi, contract.networks[networkId].address);
     return { web3, networkId, contract: deployedContract }
 }
 
-var db = admin.database();
-var ref = db.ref("Pixels");
-ref.once("value", function(snapshot) {
-    console.log(snapshot.val());
-});
-
-// async function updateEntirePixelDatabase(web3Session) {
-//     var totalElements = await web3Session.contract.methods._totalPixels().call()
-//     var pixels = []
-//     console.log(`Total elements = ${totalElements}`);
-//     totalElements = 5
-//     for (let i = 0; i < totalElements; i++) {
-//         const color = await web3Session.contract.methods._pixelColours(i).call()
-//         if (i % 100 == 0) { console.log(`Progress: ${i} / ${totalElements}`); }
-//         pixels.push(color)
-//     }
-//     console.log("pixels: ", pixels);
-//     return pixels
-// }
-
-async function run() {
+async function run_blockchain_mirror() {
     web3_instance = await initWeb3()
-    console.log(`On network: ${web3_instance.networkId}`);
-    console.log("Subbing to contract...");
-    subToTopic(web3_instance)
+    console.log(`Web3 up, network: ${web3_instance.networkId}, contract address: ${web3_instance.contract._address}`);
+    console.log("Subscribing to contract...");
+    subscribeToTopic(web3_instance)
 }
 
-async function subToTopic(web3_instance) {
+async function subscribeToTopic(web3_instance) {
     const currBlockNo = await web3.eth.getBlockNumber()
     web3_instance.contract.events.PixelColourChanged({ fromBlock: currBlockNo })
         .on("connected", function(subscriptionId) {
-            console.log("connected", subscriptionId);
+            console.log("Connected, subscription id:", subscriptionId);
         })
         .on('data', function(event) {
-            console.log("\n", event.returnValues.pixelId); // same results as the optional callback above
-            console.log(event.returnValues.newColour); // same results as the optional callback above
+            let pixelId = String(event.returnValues.pixelId)
+            let newPixelColour = String(event.returnValues.newColour)
+            console.log(`Got contract PixelColourChanged event. PixelId: ${pixelId}, new colour: ${newPixelColour}`);
+            console.log("Updating db...");
+            CACHED_DATABASE[pixelId] = newPixelColour
+            PIXEL_TABLE.update({
+                [pixelId]: newPixelColour
+            })
         })
-        .on('changed', function(event) {
-            console.log("Changed:", event);
-        })
-        .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-            console.log("Err:", error);
-        });
 }
 
-run()
-
+run_blockchain_mirror()
 
 
 
@@ -82,7 +71,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/pixel_data", (req, res) => {
-
+    res.json({ success: true, data: CACHED_DATABASE });
 })
 
 app.listen(PORT, () => {
