5a1407c7a84e8da61c50114efb87137b1b474cfd
Max <Hunt>
2021-12-25 22:52:26 +0000

    Update

--- docs/js/main.js
+++ docs/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function() {
 
 
 $('#countdown').countdown({
-    date: "Jan 30 2022",
+    date: "Jan 30 2021",
     render: function(data) {
         var el = $(this.el);
         el.empty()
