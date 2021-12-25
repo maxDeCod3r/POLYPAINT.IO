2f80659d24e5391e89ce4132231d63e7a2af337f
Max <Hunt>
2021-12-25 22:01:36 +0000

    Update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const Web3 = require('web3')
+const fs = require('fs')
+const PNG = require('pngjs').PNG
 const Contract = require('web3-eth-contract');
 const contract = require("../sol/abis/Pixels.json")
 const infuraUrl = `https://ropsten.infura.io/v3/${process.env.WEB3_INFURA_PROJECT_ID}`
 const infuraWsUrl = `wss://ropsten.infura.io/ws/v3/${process.env.WEB3_INFURA_PROJECT_ID}`
 const PORT = 3535;
+const PNG_REBUILD_INTERVAL_SECONDS = 20
+var DB_HAS_CHANGED = false
 Contract.setProvider(infuraWsUrl);
 
 
@@ -21,14 +25,23 @@ firebase_admin.initializeApp({
 var database = firebase_admin.database();
 var PIXEL_TABLE = database.ref("Pixels");
 var CACHED_DATABASE = []
+for (let i = 0; i < 1000000; i++) {
+    CACHED_DATABASE.push(0x007700)
+}
 
 async function downloadDatabase() {
     PIXEL_TABLE.once("value", function(snapshot) {
-        CACHED_DATABASE = snapshot.val();
+        var downloadedArray = snapshot.val()
+        for (const [key, value] of Object.entries(downloadedArray)) {
+            if (key >= 0) {
+                CACHED_DATABASE[key] = parseInt(value)
+            }
+        }
+        // CACHED_DATABASE = snapshot.val();
         console.log("Initial database download complete");
     });
 }
-downloadDatabase()
+
 
 
 async function initWeb3() {
@@ -59,14 +72,63 @@ async function subscribeToTopic(web3_instance) {
             PIXEL_TABLE.update({
                 [pixelId]: newPixelColour
             })
+            DB_HAS_CHANGED = true
         })
 }
 
+function splitArray(array, part) {
+    var tmp = [];
+    for (var i = 0; i < array.length; i += part) {
+        tmp.push(array.slice(i, i + part));
+    }
+    return tmp;
+}
+
+function injectTransparency(arr) {
+    arr.forEach(element => {
+
+    });
+}
+
+async function updatePng() {
+    if (DB_HAS_CHANGED) {
+        console.log('updating image');
+        image_grid = splitArray(CACHED_DATABASE, 1000)
+        createPng(image_grid)
+        DB_HAS_CHANGED = false
+    }
+}
+
+async function pngUpdator() {
+    setInterval(function() { updatePng() }, PNG_REBUILD_INTERVAL_SECONDS * 1000)
+}
+
+function createPng(imageData) {
+    var png = new PNG({
+        width: imageData[0].length,
+        height: imageData.length,
+        filterType: -1
+    });
+    for (var y = 0; y < png.height; y++) {
+        for (var x = 0; x < png.width; x++) {
+            var idx = (png.width * y + x) << 2;
+            png.data[idx] = (imageData[y][x] & 0xff0000) >> 16
+            png.data[idx + 1] = (imageData[y][x] & 0x00ff00) >> 8
+            png.data[idx + 2] = (imageData[y][x] & 0x0000ff)
+            png.data[idx + 3] = 255; // alpha (0 is transparent)
+        }
+    }
+
+    png.pack().pipe(fs.createWriteStream('latest_map.png'));
+    console.log('update done');
+}
+
+downloadDatabase()
 run_blockchain_mirror()
+pngUpdator()
 
-
-app.get("/pixel_data", (req, res) => {
-    res.json({ success: true, data: CACHED_DATABASE });
+app.get("/pixel_data.png", (req, res) => {
+    res.sendFile(__dirname + '/latest_map.png');
 })
 
 app.listen(PORT, () => {
